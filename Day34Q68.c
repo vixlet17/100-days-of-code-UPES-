fa63a573a123545d35713d9a793c86f3ed859372
vixlet17 <guptabhavya345@gmail.com>
2025-12-03 12:57:16 +0530

    Update Day34Q68.c

--- Day34Q68.c
+++ Day34Q68.c
@@ -1,11 +1,11 @@
-//Q67: Insert an element in an array at a given position.
+//Q68: Delete an element from an array.
 //
-///*
+
 
 
 #include<stdio.h>
 int main() {
-    int arr[100], n, i, element, pos;
+    int arr[100], n, i, pos;
     
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
@@ -15,25 +15,23 @@ int main() {
         scanf("%d", &arr[i]);
     }
     
-    printf("Enter element to insert: ");
-    scanf("%d", &element);
-    
-    printf("Enter position to insert (1 to %d): ", n+1);
+    printf("Enter position to delete (1 to %d): ", n);
     scanf("%d", &pos);
     
-    if(pos < 1 || pos > n+1) {
+    if(pos < 1 || pos > n) {
         printf("Invalid Position!\n");
         return 0;
     }
     
-    for(i = n; i > pos; i--) {
-        arr[i] = arr[i-1];
+    
+    
+    for(i = pos; i < n-1; i++) {
+        arr[i] = arr[i+1];
     }
     
-    arr[pos] = element;
-    n++;
+    n--; 
     
-    printf("Array after insertion:\n");
+    printf("Array after deletion:\n");
     for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
